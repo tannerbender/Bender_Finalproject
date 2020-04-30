e8c1fe735db7b3fcb9b770a3628cfe094277bb1b
tannerbender <54556211+tannerbender@users.noreply.github.com>
2020-04-30 18:07:09 -0400

    adds ball

---
+++ Bender_Finalproject/Assets/player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+
+    public class Player: MonoBehaviour{
+        public GameObject ball;
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
