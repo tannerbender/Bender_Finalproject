085a2429c5df70921a4212e27e6bdcdd6b62b76f
tannerbender <54556211+tannerbender@users.noreply.github.com>
2020-04-30 19:03:38 -0400

    adds backboard physics and rim physics, also adds ball force

---
+++ Bender_Finalproject/Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject ball;
+    public GameObject playerCamera;
+    public float ballDistance = 2f;
+    public float ballThrowingForce = 5f;
+
+    private bool holdingBall = true;
+    
+    
+    void Start()
+    {
+        ball.GetComponent<Rigidbody> ().useGravity = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (holdingBall){
+            ball.transform.position = playerCamera.transform.position + playerCamera.transform.forward * ballDistance;
+
+            if(Input.GetMouseButtonDown(0)){
+                holdingBall = false;
+                ball.GetComponent<Rigidbody> ().useGravity = true;
+                ball.GetComponent<Rigidbody> ().AddForce(playerCamera.transform.forward * ballThrowingForce);
+            }
+
+        }        
+    }
+}
